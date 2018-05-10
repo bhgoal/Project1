9db9839ad6f9f4a33099aace3c9c52c1137fe361
gorbasho <37026147+gorbasho@users.noreply.github.com>
2018-05-10 09:07:46 -0500

    hover

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -22,7 +22,7 @@ $(document).ready(iconAnim)
 function iconAnim() {
     var type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
         radius = '12em', //distance from center
-        start = -90, //shift start from 0
+        start = 0, //shift start from 0
         $elements = $('li:not(:first-child)'),
         numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
         slice = 360 * type / numberOfElements;
@@ -33,7 +33,43 @@ function iconAnim() {
             rotateReverse = rotate * -1;
         
         $self.css({
-            'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
+            'transform': 'rotate(' + rotate + 'deg) translateY(-' + radius + ') rotate(' + rotateReverse + 'deg)'
         });
     });
 }
+
+$(".signcon").hover(function() {
+    $(this).css({"transform": "translateY(-20px)"});
+}, function(){
+    $(this).css({"transform": "translateY(20px)"});
+});
+
+$("#lii").on("click", function() {
+    console.log("icon clicked");
+    var $elements = $('li');
+
+    $elements.css({
+        'left': '50px', 'top': '50px'
+    });
+
+});
+
+$("li").on("click", function() {
+    console.log("icon clicked");
+    var type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
+    radius = '12em', //distance from center
+    start = 0, //shift start from 0
+    $elements = $('li:not(:first-child)'),
+    numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
+    slice = 360 * type / numberOfElements;
+
+    $elements.each(function(i) {
+        var $self = $(this),
+            rotate = slice * i + start,
+            rotateReverse = rotate * -1;
+        
+        $self.css({
+            'transform': 'rotate(' + rotate + 'deg) translateY(-' + radius + ') rotate(' + rotateReverse + 'deg)'
+        });
+    });
+});
